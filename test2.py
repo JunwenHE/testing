dba70797672b80919581c76a25fe187bd0ee9f82
JunwenHE <30281430+JunwenHE@users.noreply.github.com>
2017-07-26 14:06:23 +1000

    test file

---
+++ test2.py
@@ -0,0 +1,4 @@
+def project_test2();
+    print("Testing 2")
+
+project_test2()
---
+++ testing.py
@@ -0,0 +1,5 @@
+def project_testing();
+    print("Testing Testing")
+    print("Tesing Branch")
+
+project_testing()
